6d84db52e7f57fb57eca8d0419c2b1781917d5f6
BhavathiR <bhavathireddy@gmail.com>
2017-09-27 23:26:49 +0530

    ID3 DONE ^_^

--- Assignment 1/src/node.cpp
+++ Assignment 1/src/node.cpp
@@ -1,8 +1,7 @@
 #include "node.h"
-
+//#include<iostream>
 #include <iterator>
 #include <math.h>
-#include<iostream>
 
 node::node(vector < adult* > plist, vector < adult* > nlist, int p, int n)
 {
@@ -46,9 +45,10 @@ void node::setbit(bitset<15> b)
 
 float node::calc_entropy()
 {
+    if(npost==0||nnegt==0) return 0;
     float ppos=(float)npost/(npost+nnegt), pneg=(float)nnegt/(npost+nnegt);
     entropy= -ppos* log2(ppos)-pneg* log2(pneg);
-    cout<<ppos<<','<<pneg<<',';
+//    cout<<ppos<<','<<pneg<<',';
 
     return entropy;
 }
@@ -56,7 +56,10 @@ float node::calc_entropy()
 void node::children_split()
 {
     for(map<string,node>::iterator it=children.begin(); it!=children.end();++it)
+    {
+        cout<<endl<<endl<<"Splitting "<<it->first<<" under "<<split_attr;
         (it->second).calc_split();
+    }
 }
 
 void node:: calc_split()
@@ -64,25 +67,28 @@ void node:: calc_split()
     if(npost==0)
     {
         split_attr="<=50K";
+        cout<<split_attr;
         return;
     }
     else if(nnegt==0)
     {
         split_attr=">50K";
+        cout<<split_attr;
         return;
     }
     int i=0;
-    float temp_ent=0, child_ent=entropy;
+    float temp_ent=0, child_ent=10000;
     string attr;
     map<string, node> temp_children;
     bitset<15> b=checked;
-    cout<<entropy;
+//    cout<<entropy;
     for(map<string, vector<string> >::iterator it=disc_values.begin(); it!=disc_values.end(); i++,it++)
     {
         if(!checked.test(i))
         {
             attr=it->first;
             temp_children.clear();
+            temp_ent=0;
             b=checked;
             b.set(i);
             temp_ent=0;
@@ -91,20 +97,21 @@ void node:: calc_split()
             for(vector<adult*>::iterator jt=neg_list.begin(); jt!=neg_list.end(); ++jt)
                 temp_children[(*jt)->ask_disc(attr)].add_neg(*jt);
             temp_children.erase("?");
-            cout<<endl<<attr<<":"<<temp_children.size()<<": ";
+//            cout<<endl<<attr<<":"<<temp_children.size()<<": ";
             for(vector<string>::iterator jt=(it->second).begin(); jt!=(it->second).end(); ++jt)
             {
-                cout<<(*jt)<<", ";
+//                cout<<(*jt)<<", ";
                 if(temp_children.count(*jt))
                 {
-                    temp_ent+=temp_children[*jt].calc_entropy();
+
+                    temp_ent+=(temp_children[*jt].calc_entropy())*(temp_children[*jt].npost+temp_children[*jt].nnegt)/(npost+nnegt);
                     temp_children[*jt].setbit(b);
                 }
             }
-            cout<<temp_ent;
+//            cout<<temp_ent;
             if(temp_ent<child_ent)
             {
-                cout<<" Selected";
+//                cout<<" Selected";
                 child_ent=temp_ent;
                 children=temp_children;
                 split_attr=attr;
@@ -117,6 +124,6 @@ void node:: calc_split()
     {
         cout<<it->first<<", ";
     }
-//    children_split();
+    children_split();
 }
 
